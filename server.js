a2c2d7c8bd6762390c9d70156052245878a014f8
unknown <you@example.com>
2021-05-30 23:12:40 +0530

    initital

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path=require("path");
 // const ejs=require("ejs");
 //Template Engine
 
+
+const cors=require("cors");
 app.set("views",path.join(__dirname,"/views"));
 app.use("/files/download",require("./routes/download.js"));
 app.set("view engine","ejs");
@@ -11,6 +13,15 @@ app.use(express.static("public"));
 app.use(express.json());
 const connectDB=require("./config/db.js");
 connectDB();
+
+//cors
+
+const corsoptions={
+    origin:process.env.ALLOWED_CLIENTS.split(',');
+    //Recieves String
+}
+app.use(cors(corsoptions));
+
 const port=process.env.PORT||3000;
 
 
